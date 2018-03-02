6a5c2327d64c8cba81f4b024532db43690f8c8af
David Styrbjörn <davidstyrbjorn@gmail.com>
2018-03-02 08:10:50 +0100

    Project Creator Work
    Copying Image Assets to project location

--- NanoGameEngineSolution/project_creator/src/main.cpp
+++ NanoGameEngineSolution/project_creator/src/main.cpp
@@ -8,14 +8,15 @@
 int main()
 {
 	// Windows.h
-	// CopyFile seems to work for both .exe and .txt files which is enough for this case
+	// CopyFile seems to work for both .exe, .txt and .png files which is enough for this case
 	// @ TODO find a function for max & linux
 	
 	const std::string inputPrompts[] = { "path: ", "name: " };
 	const std::string welcomePrompts[] = { 
 		"Welcome to the Nano Game Engine project creator", 
 		"Please enter the path for the new project ending with a \\",
-		"Please enter the name for the new project"
+		"Please enter the name for the new project (no spaces)",
+		"Please enter the projects author contact info (email, github name etc) (no spaces)"
 	};
 	std::cout << welcomePrompts[0] << std::endl;
 	std::cout << welcomePrompts[1] << std::endl;
@@ -25,30 +26,49 @@ int main()
 	std::cin >> path;
 
 	std::string name;
+	std::cout << welcomePrompts[2] << std::endl;;
 	std::cout << inputPrompts[1];
 	std::cin >> name;
 	
+	std::string author;
+	std::cout << welcomePrompts[3] << std::endl;
+	std::cout << inputPrompts[1];
+	std::cin >> author;
+	
 	std::string completeProjectPath = path + name;
+#ifdef _WINDOWS_
 	CreateDirectory(completeProjectPath.c_str(), NULL);
+#endif
 
 	std::ofstream test(completeProjectPath + "\\nano.txt", std::ios::ate);
 	/*
 	nano.txt
 	project_name string
 	author string
-	global_path string
+	local_path string
 	*/
 	test << "project_name " + name + "\n";
-	test << "author " + name + "\n";
-	test << "global_path " + completeProjectPath + "\n";
+	test << "author " + author + "\n";
+	test << "local_path " + completeProjectPath + "\n";
 
+	// Creating directories
 	std::string resourceDirectoryPath = completeProjectPath + "\\resources";
+#ifdef _WINDOWS_
 	CreateDirectory(resourceDirectoryPath.c_str(), NULL);
+#endif
 	std::string levelsDirectoryPath = resourceDirectoryPath + "\\levels";
+#ifdef _WINDOWS_
 	CreateDirectory(levelsDirectoryPath.c_str(), NULL);
+#endif
 
-	CopyFile("resources//editor.exe", std::string(completeProjectPath + "\\editor.exe").c_str(), FALSE);
+	// Copying files
+#ifdef _WINDOWS_ // Windows copy functions
+	// 1. Image assets
+	CopyFile("resources\\rect.png", std::string(completeProjectPath + "\\resources\\rect.png").c_str(), FALSE);
+	CopyFile("resources\\error_texture.png", std::string(completeProjectPath + "\\resources\\error_texture.png").c_str(), FALSE);
+	// editor.exe
+	CopyFile("resources\\editor.exe", std::string(completeProjectPath + "\\editor.exe").c_str(), FALSE);
+#endif
 
 	system("pause>null");
-	return 0;
 }
